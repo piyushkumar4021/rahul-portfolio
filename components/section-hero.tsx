f2e4571e80288c3b34d0f5818f66b8aa40fd262b
Piyush Kumar <piyushkumar4075@gmail.com>
2025-08-06 02:16:45 +0530

    done

--- components/section-hero.tsx
+++ components/section-hero.tsx
@@ -2,7 +2,7 @@ export default function SectionHero() {
   return (
     <section className="h-screen flex flex-col justify-center items-center text-center bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1600585154340-be6161a56a0c')] text-white">
       <h1 className="text-5xl font-semibold tracking-wide animate-fadeIn">
-        Namaste, I'm Rahul
+        Namaste, I&apos;m Rahul
       </h1>
       <p className="mt-2">
         Crafting Indian heritage into timeless luxury spaces.
